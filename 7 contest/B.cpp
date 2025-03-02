b2533287100aba53ec4d5513ebd94c2a5e4fdd12
GlebGerr <geraskin.gleb@gmail.com>
2025-03-02 13:51:51 +0300

    Edit B.cpp

--- 7 contest/B.cpp
+++ 7 contest/B.cpp
@@ -13,25 +13,20 @@ int main() {
         cin >> friends[i].first >> friends[i].second;
     }
 
-    // Сортируем друзей по количеству денег
     sort(friends.begin(), friends.end());
 
     long long max_friendship = 0;
     long long current_friendship = 0;
     int left = 0;
 
-    // Используем метод двух указателей
     for (int right = 0; right < n; ++right) {
-        // Добавляем текущего друга в окно
         current_friendship += friends[right].second;
 
-        // Пока разница между крайними друзьями в окне больше d, сдвигаем левый указатель
         while (friends[right].first - friends[left].first >= d) {
             current_friendship -= friends[left].second;
             left++;
         }
 
-        // Обновляем максимальную сумму
         max_friendship = max(max_friendship, current_friendship);
     }
 
